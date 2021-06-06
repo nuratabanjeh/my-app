d0e4a674226262f8ddbf3eb9a2f915e27e6b76a4
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-06 17:15:12 +0300

    done

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <nav>
+                    <ul>
+                        <li>First react App</li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+
+export default Header;
